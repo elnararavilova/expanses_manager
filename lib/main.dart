49a2f8ffa20dfde9039de7dce1e40495867e608c
elnararavilova <153461940+elnararavilova@users.noreply.github.com>
2024-06-26 23:12:47 +0300

    Update main.dart
    e

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/foundation.dart' show kIsWeb;
-import 'dart:io' show Platform;
+import 'package:provider/provider.dart';
+import 'viewmodels/transaction_viewmodel.dart';
+import 'viewmodels/theme_viewmodel.dart';
+import 'screens/home_screen.dart';
+import 'screens/expenses_screen.dart';
+import 'screens/incomes_screen.dart';
+import 'screens/add_expense_screen.dart';
+import 'screens/edit_expense_screen.dart';
+import 'screens/add_income_screen.dart';
+import 'screens/edit_income_screen.dart';
+import 'screens/expense_detail_screen.dart';
+import 'screens/income_detail_screen.dart';
+import 'screens/settings_screen.dart';
+import 'screens/network_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,43 +21,31 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Платформа приложения'),
-        ),
-        body: Center(
-          child: PlatformInfoScreen(),
-        ),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => TransactionViewModel()),
+        ChangeNotifierProvider(create: (_) => ThemeViewModel()),
+      ],
+      child: Consumer<ThemeViewModel>(
+        builder: (context, themeViewModel, child) {
+          return MaterialApp(
+            title: 'Finance App',
+            theme: themeViewModel.isDarkMode ? ThemeData.dark() : ThemeData.light(),
+            initialRoute: '/',
+            routes: {
+              '/': (context) => HomeScreen(),
+              '/expenses': (context) => ExpensesScreen(),
+              '/incomes': (context) => IncomesScreen(),
+              '/addExpense': (context) => AddExpenseScreen(),
+              '/addIncome': (context) => AddIncomeScreen(),
+              '/expenseDetail': (context) => ExpenseDetailScreen(),
+              '/incomeDetail': (context) => IncomeDetailScreen(),
+              '/settings': (context) => SettingsScreen(),
+              '/network': (context) => NetworkScreen(),
+            },
+          );
+        },
       ),
-    );
-  }
-}
-
-class PlatformInfoScreen extends StatelessWidget {
-  String getPlatformMessage() {
-    if (kIsWeb) {
-      return 'Веб-браузер';
-    } else if (Platform.isAndroid) {
-      return 'Android';
-    } else if (Platform.isIOS) {
-      return 'iOS';
-    } else if (Platform.isWindows) {
-      return 'Windows';
-    } else if (Platform.isMacOS) {
-      return 'macOS';
-    } else if (Platform.isLinux) {
-      return 'Linux';
-    } else {
-      return 'Неизвестная платформа';
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Text(
-      'Ваша платформа - ${getPlatformMessage()}',
-      style: TextStyle(fontSize: 30),
     );
   }
 }
